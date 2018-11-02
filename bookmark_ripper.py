d3d50cce2a10922cf4f0bd6d7cdd0332ce89869e
Ted Kim <ted.kim03@gmail.com>
2018-11-02 16:49:15 -0400

    Update bookmark_ripper.py

--- bookmark_ripper.py
+++ bookmark_ripper.py
@@ -1,6 +1,29 @@
 from PyPDF2 import PdfFileReader
+import pandas as pd
+import requests
+import json
 
-with open(filename, 'rb') as f:
-  p = PdfFileReader(f)
+url = "https://pdf.hres.ca/dpd_pm/"
+fulldata = []
 
-bookmarkdata = p.outlines
+df = pd.read_csv('PMs.csv', dtype=str)
+
+for row in df.pm_num:
+        r = requests.get(url+row+'.PDF')
+        if r.status_code ==200:
+                print("PM {} found!".format(row))
+                with open('file.pdf', 'wb') as temp:
+                        temp.write(r.content)
+                        print("downloading temporary file...")
+                try:
+                        with open('file.pdf', 'rb') as f:
+                                p = PdfFileReader(f)
+                                bookmark_data={row:p.outlines}
+                                fulldata.append(bookmark_data)
+                                print("PM {} bookmark extracted".format(row))
+                except Exception as e:
+                        with open('error.log','a') as log:
+                                log.write("Error on PM {}. {} {}\n".format(row,type(e),e.args))
+with open('pm_bookmark.json', 'a') as data:
+        data.write(fulldata)
+print("All bookmarks extracted.")
