58af4d5a6d84cb71e0a279b64ea1e5dfb6715327
Ted Kim <ted.kim03@gmail.com>
2018-11-05 14:22:18 -0500

    Bug fixes, QOL updates

--- bookmark_ripper.py
+++ bookmark_ripper.py
@@ -9,9 +9,11 @@ fulldata = []
 df = pd.read_csv('PMs.csv', dtype=str)
 
 for row in df.pm_num:
+        global fulldata
         r = requests.get(url+row+'.PDF')
         if r.status_code ==200:
                 print("PM {} found!".format(row))
+                print("Working on {} / {} PMs".format(counter,df.shape[0]))
                 with open('file.pdf', 'wb') as temp:
                         temp.write(r.content)
                         print("downloading temporary file...")
@@ -24,6 +26,10 @@ for row in df.pm_num:
                 except Exception as e:
                         with open('error.log','a') as log:
                                 log.write("Error on PM {}. {} {}\n".format(row,type(e),e.args))
+        else:
+                with open('error.log','a') as log:
+                        log.write('Connection error:{} on PM {}. No PM found'.format(r.status_code,row))
+fulldata = repr(fulldata)
 with open('pm_bookmark.json', 'a') as data:
-        data.write(fulldata)
+        json.dump(fulldata, data)
 print("All bookmarks extracted.")
